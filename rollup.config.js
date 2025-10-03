fdab8a752c46b3a7d06ce1d728f6eee1eba0405d
MoloMalteser <123650291+MoloMalteser@users.noreply.github.com>
2025-10-03 11:15:50 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,18 @@
-import deckyPlugin from "@decky/rollup";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 
-export default deckyPlugin({
-  // Add your extra Rollup options here
-})
+export default {
+  input: "src/index.tsx",
+  output: {
+    dir: "dist",
+    format: "esm"
+  },
+  plugins: [
+    resolve({
+      extensions: [".js", ".ts", ".tsx"]
+    }),
+    commonjs(),
+    typescript()
+  ]
+};
